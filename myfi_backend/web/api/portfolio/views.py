6739565ec819b3d93be51c0f0e2a2206443879ca
ArpitDakhera <arpitdakhera@gmail.com>
2024-03-09 15:12:28 +0530

    db-migration for portfolio api

--- myfi_backend/web/api/portfolio/views.py
+++ myfi_backend/web/api/portfolio/views.py
@@ -4,7 +4,9 @@ from uuid import UUID
 from fastapi import APIRouter, HTTPException
 from fastapi.param_functions import Depends
 from redis.asyncio import ConnectionPool
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from myfi_backend.db.dependencies import get_db_session
 from myfi_backend.services.portfolio.portfolio_service import get_portfolios
 from myfi_backend.services.redis.dependency import get_redis_pool
 from myfi_backend.utils.redis import REDIS_HASH_USER, get_from_redis
@@ -17,12 +19,14 @@ router = APIRouter()
 async def get_portfolio(
     user_id: UUID,
     redis_pool: ConnectionPool = Depends(get_redis_pool),
+    session: AsyncSession = Depends(get_db_session),
 ) -> List[PortfolioDTO]:
     """
     Retrieve portfolios based on user_id.
 
     :param user_id: The user for whom to retrieve the portfolios
     :param redis_pool: Redis connection pool.
+    :param session: Database session.
     :return: A list of portfolios for the user.
     :raises HTTPException: If the user ID is not provided or portfolios not found.
     """
@@ -33,7 +37,7 @@ async def get_portfolio(
     )
     if value is None:
         raise HTTPException(status_code=400, detail="Invalid request.")
-    portfolios = get_portfolios(user_id)
+    portfolios = await get_portfolios(session)
     if portfolios is None:
         raise HTTPException(status_code=404, detail="Portfolios not found")
     return portfolios
